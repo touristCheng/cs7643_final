b6d6a52f85b2250d61ca7f7c8cbf4c87f734a255
shuocheng <shuocheng@gatech.edu>
2024-04-09 11:09:53 -0400

    update

---
+++ install.sh
@@ -0,0 +1,12 @@
+pip3 install munch
+pip3 install open3d
+pip3 install torch
+pip3 install h5py
+pip3 install lmdb
+pip3 install msgpack_numpy
+pip3 install pytorch_lightning==0.7.1
+pip3 install hydra-core==0.11.3
+pip install plyfile
+pip install matplotlib
+pip install trimesh==2.35.39
+pip install networkx==2.2
